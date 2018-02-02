da2fd87c55f26121ac92f58fc504aa6f6ecd3b6e
wtt <wangyuanqi518@gmail.com>
2018-02-02 15:42:45 +0800

    v1.2

--- js/date.js
+++ js/date.js
@@ -219,5 +219,4 @@ class DateFn {
     }
 
 
-    
 }
---
+++ js/fun.js
@@ -0,0 +1,14 @@
+/**
+ * Ensure a function is called only once.
+ */
+export function once (fn) {
+  let called = false
+  return function () {
+    if (!called) {
+      called = true
+      fn.apply(this, arguments)
+    }
+  }
+}
+
+
