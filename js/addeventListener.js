1e88bb52486b9a5bb2792d5dfd69ef52cb6f3b9f
Aric-sky <wangyuanqi518@gmail.com>
2018-08-03 15:55:59 +0800

    eventlistener

---
+++ js/addeventListener.js
@@ -0,0 +1,15 @@
+export const on = (function() {
+  if ( document.addEventListener) {
+    return function(element, event, handler) {
+      if (element && event && handler) {
+        element.addEventListener(event, handler, false);
+      }
+    };
+  } else {
+    return function(element, event, handler) {
+      if (element && event && handler) {
+        element.attachEvent('on' + event, handler);
+      }
+    };
+  }
+})()
