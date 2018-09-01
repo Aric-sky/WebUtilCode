8c39f41399a7c64334e2c54c77fc92d596a56412
Aric-sky <wangyuanqi518@gmail.com>
2018-09-01 20:08:45 +0800

    event

--- js/addeventListener.js
+++ js/addeventListener.js
@@ -1,3 +1,4 @@
+// 监听事件api
 export const on = (function() {
   if ( document.addEventListener) {
     return function(element, event, handler) {
@@ -13,3 +14,25 @@ export const on = (function() {
     };
   }
 })()
+
+
+// pc端滚动开关
+disabledMouseWheel() {
+  document.addEventListener('DOMMouseScroll', this.scrollFunc, false)
+  document.addEventListener('mousewheel', this.scrollFunc,false)
+}
+cancelDisMouseWheel(){
+  document.removeEventListener('DOMMouseScroll', this.scrollFunc,false)
+  document.removeEventListener('mousewheel', this.scrollFunc,false)
+}
+scrollFunc(evt) {
+  evt = evt || window.event
+  if(evt.preventDefault) { // Firefox
+    evt.preventDefault()
+    evt.stopPropagation()
+  } else { // IE
+    evt.cancelBubble = true
+    evt.returnValue = false
+  }
+  return false;
+}
