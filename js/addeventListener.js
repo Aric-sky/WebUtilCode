5df2ba0e2a3a1c428c1892d4a15181eef14f7322
Aric-sky <wangyuanqi518@gmail.com>
2018-12-28 10:10:40 +0800

    修改格式

--- js/addeventListener.js
+++ js/addeventListener.js
@@ -15,38 +15,39 @@ export const on = (function() {
   }
 })()
 
-
-// pc端滚动开关
-disabledMouseWheel() {
-  document.addEventListener('DOMMouseScroll', this.scrollFunc, false)
-  document.addEventListener('mousewheel', this.scrollFunc,false)
-}
-cancelDisMouseWheel(){
-  document.removeEventListener('DOMMouseScroll', this.scrollFunc,false)
-  document.removeEventListener('mousewheel', this.scrollFunc,false)
-}
-scrollFunc(evt) {
-  evt = evt || window.event
-  if(evt.preventDefault) { // Firefox
-    evt.preventDefault()
-    evt.stopPropagation()
-  } else { // IE
-    evt.cancelBubble = true
-    evt.returnValue = false
+class Scroll {
+  // pc端滚动开关
+  disabledMouseWheel() {
+    document.addEventListener('DOMMouseScroll', this.scrollFunc, false)
+    document.addEventListener('mousewheel', this.scrollFunc,false)
   }
-  return false;
+  cancelDisMouseWheel(){
+    document.removeEventListener('DOMMouseScroll', this.scrollFunc,false)
+    document.removeEventListener('mousewheel', this.scrollFunc,false)
+  }
+  scrollFunc(evt) {
+    evt = evt || window.event
+    if(evt.preventDefault) { // Firefox
+      evt.preventDefault()
+      evt.stopPropagation()
+    } else { // IE
+      evt.cancelBubble = true
+      evt.returnValue = false
+    }
+    return false;
+  }
+
+  // 移动端touch事件阻止及放开
+
+  mo(e){
+    e.preventDefault()
+  }
+  stop(){
+    document.body.className = 'noscroll'
+    document.addEventListener("touchmove",this.mo,{ passive: false })
+  }
+  move(){
+    document.body.className = ''
+    document.removeEventListener("touchmove",this.mo,{ passive: false })
+  }
 }
-
-// 移动端touch事件阻止及放开
-
-mo(e){
-  e.preventDefault()
-},
-stop(){
-  document.body.className = 'noscroll'
-  document.addEventListener("touchmove",this.mo,{ passive: false })
-},
-move(){
-  document.body.className = ''
-  document.removeEventListener("touchmove",this.mo,{ passive: false })
-},
--- js/share.js
+++ js/share.js
@@ -1,85 +1,87 @@
 
-//share 分享首次判断
-isShare:function(ykId){
-  var day = this.formatTime(), isShare = this.localGet('isShare');
-  if (isShare == null || isShare == undefined) {
-    return true;
-  }else{
-    if(!this.has(isShare, ykId)){
-      return true
-    } else { 
-      if(this.has(isShare[ykId], day)){
-        return false
-      }else{
+class Share {
+  //share 分享首次判断
+  isShare (ykId) {
+    var day = this.formatTime(), isShare = this.localGet('isShare');
+    if (isShare == null || isShare == undefined) {
+      return true;
+    }else{
+      if(!this.has(isShare, ykId)){
         return true
-      }
-    }
-  }
-},
-
-//share set ,daliy share prove
-shareSet:function(ykId){
-  var day = this.formatTime()
-  this.localSet('isShare', ykId, day)
-},
-
-formatTime: function(date){
-  var dateSelf = date || new Date();
-  var arr = [dateSelf.getFullYear(), dateSelf.getMonth() + 1, dateSelf.getDate()];
-  return arr.join('');
-},
-
-//设置单个key 导出
-localSet:function(item,key,val){
-  var dataList = {};
-  if (this.localGet(item)) {
-    dataList = this.localGet(item);
-  }
-  if(dataList[key] && this.typeCheck(dataList[key]) === "Array"){
-    dataList[key].push(val);
-  }else{
-    dataList[key] = [val];
-  }
-  window.localStorage.setItem(item, this.stringly(dataList));
-},
-
-has: function(arg, item){  //true 包含, false 不含
-  var type = this.typeCheck(arg);
-  if (type === 'object') {
-    return arg.hasOwnProperty(item);
-  }else if (type === 'Array') {
-    if(arg.length > 0 ){
-      for(var i= 0, len = arg.length; i< len; i++){
-        if(arg[i] == item){
-          return true;
+      } else { 
+        if(this.has(isShare[ykId], day)){
+          return false
+        }else{
+          return true
         }
       }
-      return false;
+    }
+  }
+
+  //share set ,daliy share prove
+  shareSet (ykId) {
+    var day = this.formatTime()
+    this.localSet('isShare', ykId, day)
+  }
+
+  formatTime (date) {
+    var dateSelf = date || new Date();
+    var arr = [dateSelf.getFullYear(), dateSelf.getMonth() + 1, dateSelf.getDate()];
+    return arr.join('');
+  }
+
+  //设置单个key 导出
+  localSet (item,key,val) {
+    var dataList = {};
+    if (this.localGet(item)) {
+      dataList = this.localGet(item);
+    }
+    if(dataList[key] && this.typeCheck(dataList[key]) === "Array"){
+      dataList[key].push(val);
     }else{
-      return false ;
+      dataList[key] = [val];
+    }
+    window.localStorage.setItem(item, this.stringly(dataList));
+  }
+
+  has (arg, item) {  //true 包含, false 不含
+    var type = this.typeCheck(arg);
+    if (type === 'object') {
+      return arg.hasOwnProperty(item);
+    }else if (type === 'Array') {
+      if(arg.length > 0 ){
+        for(var i= 0, len = arg.length; i< len; i++){
+          if(arg[i] == item){
+            return true;
+          }
+        }
+        return false;
+      }else{
+        return false ;
+      }
     }
   }
-},
 
-// 判断数据类型
-typeCheck: function (obj) {
-  var objType = typeof (obj);
-  if (objType == "object") {
-    if (obj instanceof Array) { // 当为array时
-      objType = "Array"
+  // 判断数据类型
+  typeCheck (obj) {
+    var objType = typeof (obj);
+    if (objType == "object") {
+      if (obj instanceof Array) { // 当为array时
+        objType = "Array"
+      }
     }
+    return objType
   }
-  return objType
-},
 
-//获得整个item的内容 导出
-localGet:function(item){
-  return this.parsely(window.localStorage.getItem(item));
-},
+  //获得整个item的内容 导出
+  localGet (item) {
+    return this.parsely(window.localStorage.getItem(item));
+  }
 
-stringly:function(obj){
-  return JSON.stringify(obj);
-},
-parsely:function(obj){
-  return JSON.parse(obj);
-},
+  stringly (obj) {
+    return JSON.stringify(obj);
+  }
+  parsely(obj) {
+    return JSON.parse(obj);
+  }
+}
