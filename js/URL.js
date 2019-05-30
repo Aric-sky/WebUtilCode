ecdd123a297267490d671baad23e77cfce1aaa51
Aric-sky <wangyuanqi518@gmail.com>
2019-05-30 15:30:21 +0800

    Update URL.js

--- js/URL.js
+++ js/URL.js
@@ -38,6 +38,12 @@ const parse_url = (url) => {
   return parames;
 }
 
-
+// 获得URL中的指定参数
+function getQueryString(name) { 
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+    var r = window.location.search.substr(1).match(reg); 
+    if (r != null) return unescape(r[2]); 
+    return null; 
+} 
 
 
