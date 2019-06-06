9a9198bab3e8ee212f3fc1a4ed1256d3b50819f5
Aric-sky <wangyuanqi518@gmail.com>
2019-06-06 15:30:19 +0800

    Create random.js

---
+++ js/random.js
@@ -0,0 +1,17 @@
+
+  function createId() {
+    return (
+      (Math.random() * 10000000).toString(16).substr(0, 4) +
+      "-" +
+      new Date().getTime() +
+      "-" +
+      Math.random()
+        .toString()
+        .substr(2, 5)
+    );
+  }
+
+
+module.exports = {
+  createId
+}
